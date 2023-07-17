ad4881a58d35c4e231b0a6e8bf23c5ed1b979cd1
Demo User <demo.user.0186@gmail.com>
2023-07-17 17:44:38 +0300

    small changes to kolobok

--- filter-sample.js
+++ filter-sample.js
@@ -1,5 +1,5 @@
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present', 'Kouzma'];
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
-const result = words.filter(word => word.length < 7);
+const result = words.filter(word => word.length >= 7);
 
 console.log(result);
